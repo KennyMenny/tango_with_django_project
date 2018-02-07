b938fbea2b3ed117d7fad74069d97b928c08e788
Ken Diasilua <2263031d@student.gla.ac.uk>
2018-02-07 15:27:29 +0000

    chapter 5(full final)

--- populate_rango.py
+++ populate_rango.py
@@ -28,18 +28,16 @@ def populate():
         {"title": "Flask",
          "url": "http://flask.pocoo.org"}]
 
-    cats = {"Python": {"pages":python_pages, "views":c.views, "likes":c.likes},
-         "Django": {"pages":django_pages, "views":c.views, "likes":c.likes},
-         "Other Frameworks": {"pages":other_pages, "views":c.views, "likes":c.likes}}
+    cats = {"Python": {"pages":python_pages, "views":128, "likes":64},
+         "Django": {"pages":django_pages, "views":64, "likes":32},
+         "Other Frameworks": {"pages":other_pages, "views":32, "likes":16}}
 
 
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat, views, likes)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
-            for k in cat_data["views"]:
-                for i in cat_data["likes"]:
-                    add_page(c, p["title"], p["url"], k["views"], i["likes"])
+                add_page(c, p["title"], p["url"])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -53,7 +51,9 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name, views, likes):
-    c=Category.objects.get_or_create(name=name, views = c.views, likes = c.likes)[0]
+    c=Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
